38f712d00c432c4376c9ee1fb931c4a52f3a3e3d
stubbornick <38818331+stubbornick@users.noreply.github.com>
2020-11-09 21:40:15 +0200

    Rates fetching

--- src/db.js
+++ src/db.js
@@ -1,9 +1,19 @@
-const mongoose = require (mongoose);
+const mongoose = require('mongoose');
 
 const assetScheme = new mongoose.Schema({
   symbol: String,
 });
 
+const rateScheme = new mongoose.Schema({
+  asset: String,
+  assetPrice: Number,
+  platform: String,
+  rate: Number,
+  borrowRate: Number,
+  totalSupply: Number,
+});
+
 module.exports = {
   assetScheme,
+  rateScheme,
 };
--- src/fetch_script.js
+++ src/fetch_script.js
@@ -2,25 +2,54 @@ require('dotenv/config');
 const axios = require('axios');
 const mongoose = require('mongoose');
 
-const { assetScheme } = require('./db');
+const { assetScheme, rateScheme } = require('./db');
 
 const defiApiKey = process.env.DEFI_API_KEY;
 const mongoUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1/data-api';
+const investAmount = 1000;
 
 const Asset = mongoose.model('Asset', assetScheme);
+const Rate = mongoose.model('Rate', rateScheme);
 
 const main = async () => {
   await mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
-  // Fetch assets
+  // Refetch assets
+  await Asset.deleteMany({});
+
   const res1 = await axios.get('https://public.defipulse.com/api/GetLendingTokens', {
     params: {
       'api-key': defiApiKey,
     },
   });
   const assetsSymbols = res1.data;
-  await Asset.deleteMany({});
   await Asset.insertMany(assetsSymbols.map((symbol) => new Asset({ symbol })));
+
+  // Refetch rates for all symbols
+  await Rate.deleteMany({});
+
+  for (const symbol of assetsSymbols) {
+    console.log(`Process asset: '${symbol}'`);
+
+    const { data } = await axios.get('https://public.defipulse.com/api/GetRates', {
+      params: {
+        token: symbol,
+        amount: investAmount,
+        'api-key': defiApiKey,
+      },
+    });
+
+    await Rate.insertMany(Object.values(data.rates).map(
+      (platform) => new Rate({
+        asset: data.token.name,
+        assetPrice: data.token.price,
+        platform: platform.name,
+        rate: platform.lend.rate,
+        borrowRate: platform.borrow.rate,
+        totalSupply: platform.supply,
+      }),
+    ));
+  }
 
   await mongoose.connection.close();
 };
