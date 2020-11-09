8051ed4a15eabb6c796be125b5906f457975276c
stubbornick <38818331+stubbornick@users.noreply.github.com>
2020-11-09 21:50:37 +0200

    API endpoints for assets and rates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 require('dotenv/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
+const { assetScheme, rateScheme } = require('./db');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1/data-api';
+
+const Asset = mongoose.model('Asset', assetScheme);
+const Rate = mongoose.model('Rate', rateScheme);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('It works!');
 });
+
+app.get('/assets', async (req, res) => {
+  const assets = await Asset.find();
+  res.send(assets.map(a => a.symbol));
+});
+
+app.get('/rates', async (req, res) => {
+  const rates = await Rate.find();
+  res.send(rates);
+});
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
